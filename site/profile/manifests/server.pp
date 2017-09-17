a1b249c8753c53885a89c1417238954eada7ffbf
Alexander Hermes <alexander.hermes@alnx.org>
2017-09-17 16:55:16 +0100

    Fix jail=>jails typo

--- site/profile/manifests/server.pp
+++ site/profile/manifests/server.pp
@@ -6,12 +6,12 @@
 #   include profile::server
 # @param fail2ban_jail List of fail2ban jails to enable on this server
 class profile::server (
-  Array[String] $fail2ban_jail = ['sshd', 'sshd-ddos'],
+  Array[String] $fail2ban_jails = ['sshd', 'sshd-ddos'],
 ) {
   # Note that "Core" is the LSB name of CentOS 7 so this might not work on all distros
   # cf. https://forge.puppet.com/dhoppe/fail2ban/readme
   class { 'fail2ban':
     config_file_template => 'fail2ban/Core/etc/fail2ban/jail.conf.erb',
-    jails => $fail2ban_jail,
+    jails => $fail2ban_jails,
   }
 }
